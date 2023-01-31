b001170d5630f0a772102582ca1c3dd9610a7653
Jack Lawthom <jlawthom@liveperson.com>
2023-01-31 20:59:48 +0000

    Update .env

--- env.js
+++ env.js
@@ -1,8 +1,4 @@
-// Local Dev = localhost:8080/CompTrak/
-// Staging = CompTrak-Dev.lawthom.tech
-// Production = CompTrak.lawthom.tech
 const active_dir = location.origin + "/";
 const login_redirect_uri = location.origin + "/Auth/Login_Redirect.html";
 const logout_redirect_uri = location.origin + "/Auth/Logout_Redirect.html";
-
 const api_stage = window.location.hostname.includes('-dev') ? "dev" : "prod";

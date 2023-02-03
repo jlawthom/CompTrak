4a76f787458de32290795bb87626bdaf521a84a9
Jack Lawthom <jlawthom@liveperson.com>
2023-02-03 23:19:49 +0000

    Updated API path variables

--- Constants/Constants.js
+++ Constants/Constants.js
@@ -5,18 +5,17 @@ const TOURNAMENT_TREE_HTML = "Tournament%20Tree";
 const ACCOUNT_HTML = "Account";
 
 const ACTIVE_URL = "http://35.234.133.160:8081";
-const API_CALLER = `.execute-api.eu-west-2.amazonaws.com/${api_stage}/`;
+const API_CALLER = `.execute-api.eu-west-2.amazonaws.com/${api_stage}`;
 
 const TOURNAMENTS_DOMAIN = "https://t6qd3i7fbg";
 const MATCHES_DOMAIN = "https://vwhrk63drl";
 const GROUPS_DOMAIN = "https://ub59ci9j2m";
 
-const CRT_USER = "/User/create";
-const DEL_USER = "/User/delete/";
-const UPD_USER = "/User/update/";
-const GET_USERS = "/User/getAll";
-const LOGIN_USER = "/User/login/";
-const GET_USR_BY_ID = "/User/getById/";
+const TOURNAMENT_API_PATH = "/tournament/";
+const TOURNAMENTS_BY_USER_API_PATH = "/tournaments/user/";
+const MATCHES_BY_TOURNAMENT_API_PATH = "/matches/tournament/";
+const GROUP_API_PATH = "/group/";
+const MATCH_API_PATH = "/match/";
 
 const PRODUCT_TITLE = "CompTrak";
 

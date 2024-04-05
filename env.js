b0ed43cb27c70f00ad4e3fc694c48a341bcac463
jlawthom <71012108+jlawthom@users.noreply.github.com>
2024-04-05 16:08:52 +0100

    Update env.js

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
-const active_dir = location.origin + "/";
+const active_dir = location.origin + "/CompTrak/";
 const login_redirect_uri = location.origin + "/Auth/Login_Redirect.html";
 const logout_redirect_uri = location.origin + "/Auth/Logout_Redirect.html";
 const api_stage = sessionStorage.getItem('api_stage') || "prod";
